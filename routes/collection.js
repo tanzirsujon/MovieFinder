16fa1e18ac26fbc7bfead04a2d36bc4bddf9c9c0
Ajijur <ajijurrahmansujon@gmail.com>
2024-12-05 12:01:52 +0600

    add css to collection

--- routes/collection.js
+++ routes/collection.js
@@ -7,7 +7,7 @@ const path = require('path');
 router.get('/', (req, res) => {
 
     res.sendFile(path.join(__dirname, '..', 'views', 'moiveCollection.html'));
-    console.log(path.join(__dirname, '..', 'views', 'moiveCollection.html'))
+    // console.log(path.join(__dirname, '..', 'views', 'moiveCollection.html'))
 })
 
 
